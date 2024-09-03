eec9758655f16c415998bd11e2f972d4a731167b
FMA07 <133886882+FMA07@users.noreply.github.com>
2024-09-03 09:56:28 -0400

    Add files via upload

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -21,6 +21,17 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
+  async presentToast(message:string, duration?:number){
+    const toast = await this.toastController.create(
+      {
+        message:message,
+        duration:duration?duration:2000,
+        position: 'top'
+      }
+    );
+    toast.present();
+  }
+
   validateModel(model:any){
     for(var [key, value] of Object.entries(model)){
       if (value=="") {
@@ -31,6 +42,42 @@ export class LoginPage implements OnInit {
     return true;
   }
   
-  validarLargoUsuario(dato: String)
+  validarLargoUsuario(dato: string){
+    if(dato.length>=3 && dato.length<=8){
+      return true
+    }
+    return false;
+  }
 
+  validarLargoPass(dato: string){
+    if(dato.length>=8){
+      return true
+    }
+    return false;
+  }
+
+  iniciarSesion(){
+    if(this.validateModel(this.login)){
+      if(this.validarLargoUsuario(this.login.Usuario)){
+        if(this.validarLargoPass(this.login.Password)){
+          this.presentToast("Bienvenido "+ this.login.Usuario);
+          let navigationExtras:NavigationExtras={
+            state: {user:this.login.Usuario}
+          }
+          this.router.navigate(['home'], navigationExtras);
+        }else{
+          this.presentToast("La contraseña debe ser de largo 8");
+          this.login.Password="";
+          let navigationExtras: NavigationExtras={
+            state:{password: this.login.Password}
+          }
+        }
+      }else{
+        this.presentToast("El largo del usuario debe ser entre 3 y 8 carácteres");
+        this.login.Usuario="";
+      }
+    }else{
+      this.presentToast("Falta: "+ this.field);
+    }
+  }
 }
--- src/app/reset-password/reset-password.page.ts
+++ src/app/reset-password/reset-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPage implements OnInit {
 
-  constructor() { }
+  Usuario: string="";
+  Password: string="";
 
-  ngOnInit() {
+  constructor(private router: Router) { }
+
+  ngOnInit(){
+    const state = this.router.getCurrentNavigation()?.extras.state;
+
+    if (state && state['password']){
+      this.Password = state['password'];
+      
+    }else{
+      this.Password = history.state.password;
+    }
+  }
+
+  validarLargoUsuario(dato: string){
+    if(dato.length>=3 && dato.length<=8){
+      return true
+    }
+    return false;
+  }
+
+  recuperarPass(){
+    if(this.validarLargoUsuario(this.Usuario)){}
   }
 
 }
