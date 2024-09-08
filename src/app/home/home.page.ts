2aa95d56d39812f090b310caa3ef2b271f762819
Pepinillo2020 <150717538+Pepinillo2020@users.noreply.github.com>
2024-09-07 21:24:47 -0400

    Add files via upload
    Cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   username: string = '';
 
-  constructor(private router:Router){ }
+  constructor(private router: Router, private alertController: AlertController) {}
 
-  ngOnInit(){
+  ngOnInit() {
     const state = this.router.getCurrentNavigation()?.extras.state;
 
-    if (state && state['user']){
+    if (state && state['user']) {
       this.username = state['user'];
-      
-    }else{
+    } else {
       this.username = history.state.user;
     }
+  }
+
+  async registerAttendance() {
+    const alert = await this.alertController.create({
+      header: 'Asistencia Registrada',
+      message: 'Tu asistencia ha sido registrada con éxito.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
