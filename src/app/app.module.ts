1580ea869f8fff1308d80dfe296a8c504eae839b
FMA07 <133886882+FMA07@users.noreply.github.com>
2024-10-01 12:03:29 -0300

    App.module actualizado
    Cambiado para incorporar los modulos de docente y qr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DocenteComponentComponent } from './docente-component/docente-component.component';
+import { QrCodeComponentComponent } from './qr-code-component/qr-code-component.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, QrCodeComponentComponent, DocenteComponentComponent],
   imports: [BrowserModule, BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
